6e23ce1e4913df15fcf8de6bb72fb8d09fc6530b
Abdulazeez Olawale <orionabq@gmail.com>
2026-01-15 21:29:15 +0100

    completed the development of the footer section of the landing page

--- app/components/sections/Footer.tsx
+++ app/components/sections/Footer.tsx
@@ -1,9 +1,17 @@
 import CurrencyStack from "@/app/assets/icons/CurrencyStack";
 import Play from "@/app/assets/icons/Play";
-import { Quote } from "lucide-react";
+import {
+  ArrowBigLeft,
+  ArrowBigRightDash,
+  Dot,
+  MoveRight,
+  Quote,
+} from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import BiccasButton from "../common/BiccasButton";
+import Logo from "@/app/assets/icons/logo";
+import Link from "next/link";
 
 const Footer = () => {
   const testimonialsImage = [
@@ -13,8 +21,31 @@ const Footer = () => {
     "/images/testi-4.png",
   ];
 
+  const footerLinks = [
+    {
+      header: "Support",
+      body: ["Help centre", "Account information", "About", "Contact us"],
+    },
+    {
+      header: "Help and Solution",
+      body: [
+        "Talk to support",
+        "Support docs",
+        "System status",
+        "Covid responde",
+      ],
+    },
+    {
+      header: "Product",
+      body: ["Update", "Security", "Beta test", "Pricing product"],
+    },
+  ];
+
+  // this gets the current year
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="bg-[#161C28] ">
+    <footer className="bg-[#161C28] flex flex-col gap-10">
       <section className="container container-space">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           <div className="flex flex-col gap-4">
@@ -81,10 +112,64 @@ const Footer = () => {
                 ></textarea>
               </div>
 
-              <BiccasButton title="Request Demo" classname="rounded-md! w-full" />
+              <BiccasButton
+                title="Request Demo"
+                classname="rounded-md! w-full"
+              />
             </form>
           </div>
         </div>
+      </section>
+
+      <section className="container container-space flex flex-col gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="flex flex-col justify-between">
+            <Logo />
+            <p>Get started now try our product</p>
+
+            <div className="border border-white w-[410px] flex flex-row rounded-full overflow-hidden relative">
+              <input
+                type="text"
+                placeholder="Enter your email here"
+                className="placeholder:text-lg placeholder:text-(--text-gray) py-4 indent-4 w-full h-full text-white pr-[56px]"
+              />
+              <div className="absolute top-[50%] -translate-[50%] -right-4.5">
+                <button className="bg-(--primary-color) size-12 rounded-full flex items-center justify-center border border-transparent hover:bg-transparent hover:border hover:border-(--primary-color) transition-all ease-in-out cursor-pointer">
+                  <MoveRight className="text-white" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-row items-center justify-between">
+            {footerLinks.map((item, index) => (
+              <div key={index} className="flex flex-col gap-6">
+                <span className="text-white text-lg">{item.header}</span>
+                <ul className="flex flex-col gap-3">
+                  {item.body.map((footerBodyItem, index) => (
+                    <li
+                      key={index}
+                      className="list-none text-(--text-gray) hover:text-white transition-all ease-in-out"
+                    >
+                      <Link href="#">{footerBodyItem}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <section className="flex flex-row items-center justify-between translate-y-8">
+          <p className="text-white">
+            &copy; {currentYear} Biccas Inc. Copyright and rights reserved
+          </p>
+          <div className="flex flex-row gap-2 text-white">
+            <span>Terms and Condtions</span>
+            <Dot className="text-(--text-gray)" />
+            <span>Privacy Policy</span>
+          </div>
+        </section>
       </section>
     </footer>
   );
